2a87e84056a13978520665d40de5820bb05afb2a
SamuelSemlan <semlan12@live.com>
2017-03-09 14:21:57 +0100

    Diagnos

--- diagnos.py
+++ diagnos.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask("diagnos")
 
 
 @app.route("/")
 def home():
-	return "Hello world!" 
+    return render_template("home.html") 
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        return "Tack"
+    else:
+        return render_template("contact.html") 
 
 
 if __name__ == "__main__":
-	app.run()
+    app.run("0.0.0.0", debug=True)
